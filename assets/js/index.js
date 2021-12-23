2169b57674a8c1a628a6cd44dbea2cd0463cc118
Cesar Zubilete <brizusan@gmail.com>
2021-12-23 05:08:43 -0500

    Section Header
    header and nav responsive

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+//menu botom y search
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
+
+searchBtn.onclick = ()=>{
+    searchBtn.classList.toggle('fa-times');
+    searchForm.classList.toggle('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = ()=>{
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = ()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+}
+window.onscroll = ()=>{
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
