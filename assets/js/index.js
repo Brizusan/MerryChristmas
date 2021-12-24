cec84e3a12da593804f827919183287975d35cdb
Cesar Zubilete <brizusan@gmail.com>
2021-12-24 01:36:55 -0500

    Home Section
    *Home section;
    * desktop and Mobile .
    *slider.

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,5 +31,21 @@ window.onscroll = ()=>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+//Section Home y Slider
+let slides = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
 
 
